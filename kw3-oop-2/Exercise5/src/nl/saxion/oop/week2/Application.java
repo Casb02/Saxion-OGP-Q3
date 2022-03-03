f8e476b43919aabb02ba73cafc9f8e6e5ef7f5b1
Cas Bookamp <casb0002@gmail.com>
2022-03-03 13:16:47 +0100

    Yathzee maybe done bad but dont care

--- kw3-oop-2/Exercise5/src/nl/saxion/oop/week2/Application.java
+++ kw3-oop-2/Exercise5/src/nl/saxion/oop/week2/Application.java
@@ -2,6 +2,8 @@ package nl.saxion.oop.week2;
 
 import nl.saxion.app.SaxionApp;
 
+import java.awt.*;
+
 public class Application implements Runnable {
 	public static void main(String[] args) {
 		SaxionApp.start(new Application());
@@ -9,21 +11,21 @@ public class Application implements Runnable {
 
 	public void run() {
 		// TODO: Write your own implementation. For a better idea on what to do, we have provided a sample implementation for the run() method.
-//		SaxionApp.setBackgroundColor(Color.gray);
+		SaxionApp.setBackgroundColor(Color.gray);
 
-//		Yahtzee yahtzee = new Yahtzee();
+		Yahtzee yahtzee = new Yahtzee();
 
-//		while (true) {
-//			SaxionApp.clear();
-//
-//			int[] diceOutcomes = yahtzee.roll();
-//			yahtzee.draw(); // Visualize the dice results!
-//
-//			String combination = yahtzee.determineCombination(diceOutcomes);
-//			SaxionApp.drawText(combination, 10, 200, 40);
-//
-//
-//			SaxionApp.pause();
-//		}
+		while (true) {
+			SaxionApp.clear();
+
+			int[] diceOutcomes = yahtzee.roll();
+			yahtzee.draw(); // Visualize the dice results!
+
+			String combination = yahtzee.determineCombination(diceOutcomes);
+			SaxionApp.drawText(combination, 90, 200, 40);
+
+
+			SaxionApp.pause();
+		}
 	}
 }
