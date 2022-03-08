38cfb43dc073d9d9882b502e7fe3817cb8b742de
Cas Bookamp <casb0002@gmail.com>
2022-03-08 15:35:17 +0100

    Added Week 4.1

--- kw3-oop-4/Exercise1/src/nl/saxion/oop/week4/Application.java
+++ kw3-oop-4/Exercise1/src/nl/saxion/oop/week4/Application.java
@@ -1,6 +1,10 @@
 package nl.saxion.oop.week4;
 
 import nl.saxion.app.SaxionApp;
+import nl.saxion.oop.week4.models.Shape;
+import nl.saxion.oop.week4.models.shapes.Circle;
+import nl.saxion.oop.week4.models.shapes.Rectangle;
+import nl.saxion.oop.week4.models.shapes.Square;
 
 import java.awt.*;
 
@@ -12,19 +16,19 @@ public class Application implements Runnable {
     public void run() {
 
         // The line below is the key to this exercise.
-//        Shape[] shapes = { new Circle(500, 350, 150, Color.RED), new Rectangle(300, 300, 400, 250, Color.BLUE), new Square(300, 300, 200, Color.GREEN)};
+        Shape[] shapes = { new Circle(500, 350, 150, Color.RED), new Rectangle(300, 300, 400, 250, Color.BLUE), new Square(300, 300, 200, Color.GREEN)};
 
-//        for(Shape s : shapes) {
-//            SaxionApp.clear();
-//
-//            SaxionApp.printLine("Type of shape: " + s.getClass().getSimpleName());
-//
-//            SaxionApp.printLine("Circumference: " + s.getCircumference());
-//            SaxionApp.printLine("Area: " + s.getArea());
-//
-//            s.draw();
-//
-//            SaxionApp.pause();
-//        }
+        for(Shape s : shapes) {
+            SaxionApp.clear();
+
+            SaxionApp.printLine("Type of shape: " + s.getClass().getSimpleName());
+
+            SaxionApp.printLine("Circumference: " + s.getCircumference());
+            SaxionApp.printLine("Area: " + s.getArea());
+
+            s.draw();
+
+            SaxionApp.pause();
+        }
     }
 }
